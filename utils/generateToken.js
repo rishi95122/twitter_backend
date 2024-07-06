a59c721553e66ccba4fe42a96ac0f64c2eed6f0c
Uditya Kumar <74761401+rishi95122@users.noreply.github.com>
2024-07-06 14:13:48 +0530

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true,
+		
 		secure: true, // Ensure this is true in production
 		sameSite: 'None'
 	});
