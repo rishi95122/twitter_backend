281488363bc4d2771c935dc48f2e6f7ea6cf47a8
Uditya Kumar <74761401+rishi95122@users.noreply.github.com>
2024-07-06 16:19:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 const corsOptions = {
-    origin: 'https://tweeet-two.vercel.app',  // Adjust to your frontend domain
+    origin: '*',  // Adjust to your frontend domain
     credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'Content-Type, Authorization'
